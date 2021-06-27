d8d66cab282ec7969bcbf80953f82b0946c35388
Leo Zeng <qingchun1995@gmail.com>
2021-06-27 12:12:26 +0800

    edit Prettier configuration file

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  // no trailing commas at the end
+  trailingComma: 'none',
   // add semicolon at the end of the line
   semi: true,
   // use single quotes
-  singleQuote: true,
+  singleQuote: true
 };
