bb1c60a263e20dd1bce1f70d46d7bee76e3be44c
Leo Zeng <qingchun1995@gmail.com>
2021-07-05 16:40:54 +0800

    reduce verbose code

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,36 @@
 #!/usr/bin/env node
 
-import { baiduTranslate, youdaoTranslate, googleTranslate } from './main';
 import { Command } from 'commander';
 import dotenv from 'dotenv';
 import pkg from '../package.json';
+import { baiduTranslate, googleTranslate, youdaoTranslate } from './main';
 
 dotenv.config();
 
 const program = new Command();
+const options = {
+  baidu: {
+    command: 'baidu <word>',
+    description: 'use Baidu Translate',
+    action: (word: string) => {
+      baiduTranslate(word)
+    }
+  },
+  youdao: {
+    command: 'youdao <word>',
+    description: 'use Youdao Translate',
+    action: (word: string) => {
+      youdaoTranslate(word)
+    }
+  },
+  google: {
+    command: 'google <word>',
+    description: 'use Google Translate',
+    action: (word: string) => {
+      googleTranslate(word)
+    }
+  }
+};
 
 program
   .version(pkg.version)
@@ -15,19 +38,11 @@ program
   .usage('<tool> <word>') // * Usage: facile-translation <tool> <word>
   .helpOption('-H, --help', 'read more information');
 
-program
-  .command('baidu <word>')
-  .description('use Baidu Translate')
-  .action((word: string) => baiduTranslate(word));
-
-program
-  .command('youdao <word>')
-  .description('use Youdao Translate')
-  .action((word: string) => youdaoTranslate(word));
-
-program
-  .command('google <word>')
-  .description('use Google Translate')
-  .action((word: string) => googleTranslate(word));
+Object.values(options).forEach((option) => {
+  program
+    .command(option.command)
+    .description(option.description)
+    .action(option.action);
+});
 
 program.parse();
