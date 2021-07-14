ce640e8c34eb209417cc226cb30b119f11402477
Leo Zeng <qingchun1995@gmail.com>
2021-07-14 15:59:35 +0800

    feat: 🎸 translate Chinese-English and English-Chinese
    translate from Chinese to English and from English to Chinese

--- src/main.ts
+++ src/main.ts
@@ -11,22 +11,26 @@ type BaiduResult = {
   error_msg?: string;
 };
 
+const pattern = new RegExp('[\u4E00-\u9FA5]+');
 const errorMap = {
   52003: 'UNAUTHORIZED USER',
   54001: 'Invalid Sign',
+  54003: '访问频率受限',
+  54004: '账户余额不足',
   unknown: 'Internal Server Error'
-}
+};
 
 const baiduTranslate = (word: string) => {
   const salt = Math.random();
-  const appId = process.env.BAIDU_APP_ID + '1';
+  const appId = process.env.BAIDU_APP_ID;
   const appKey = process.env.BAIDU_APP_KEY;
   const sign = md5(`${appId}${word}${salt}${appKey}`);
+  const isChinese = pattern.test(word); // * Verify that it is in Chinese
 
   const parsedQuery: string = querystring.stringify({
     q: word,
-    from: 'en',
-    to: 'zh',
+    from: isChinese ? 'zh' : 'en',
+    to: isChinese ? 'en' : 'zh',
     appid: appId,
     salt,
     sign
@@ -49,7 +53,7 @@ const baiduTranslate = (word: string) => {
     response.on('end', () => {
       const string = Buffer.concat(chunks).toString();
       const object: BaiduResult = JSON.parse(string);
-      const { error_code, error_msg } = object
+      const { error_code, error_msg } = object;
 
       if (error_code) {
         console.log('Error:', errorMap[error_code] || error_msg);
