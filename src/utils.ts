06200ec1d10d2d5904c016c03ed3a9b412766c9f
Leo Zeng <qingchun1995@gmail.com>
2021-07-17 21:56:23 +0800

    refactor: 💡 folder structure

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-export const truncate = (query: string) => {
-  const len = query.length;
-
-  if (len <= 20) {
-    return query;
-  } 
-
-  return query.substring(0, 10) + len + query.substring(len - 10, len);
-};
